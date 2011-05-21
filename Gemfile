cffde6b04d250e70a55ac3e1c23bb9d4fce5e0ca
Matt Manning <matt.manning@gmail.com>
2011-05-21 15:18:55 -0400

    Add Hoptoad.

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source :rubygems
 gem 'sinatra'
 gem 'em-http-request'
 gem 'sinatra-redis', :require => 'sinatra/redis'
+gem 'hoptoad_notifier'
--- tweetclock.rb
+++ tweetclock.rb
@@ -3,7 +3,14 @@ require 'eventmachine'
 require 'em-http'
 require 'json'
 require 'sinatra/redis'
+require 'hoptoad_notifier'
 
+HoptoadNotifier.configure do |config|
+  config.api_key = ENV['HOPTOAD_API_KEY']
+end
+
+use HoptoadNotifier::Rack
+enable :raise_errors
 set :redis, ENV['REDISTOGO_URL']
 
 get '/' do
